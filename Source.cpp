755901b76578dc25a47f9c1c9a67a6cf080f57b0
123445671qaz <51878024+123445671qaz@users.noreply.github.com>
2019-06-16 18:47:08 +0700

    rand

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,20 @@
 #include <iostream> 
+#include <cstdlib>
+#include <ctime> // не  привязывает  к  определен. числам
 int main() {
-	std::cout << 1;
+	setlocale(LC_ALL, "");
+	using namespace std;
+	srand(time(0));
+	int mas[50] ;
+	for (int a=0; a < 50; a++) {
+
+	}
+	
+		system("pause");
 	return 0;
 }
+
+	/*
+	int b = 10 + rand() % 20;
+		int a = 10 + rand() % 20
+		*/
